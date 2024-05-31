fd233094efb9dd956812c06ae57f85af1efaae37
GPalma07 <108351480+GPalma07@users.noreply.github.com>
2024-05-31 16:10:10 -0600

    Update AutoresLibrosContext.cs

--- Autores_Libros.DaraAccess/Models/AutoresLibrosContext.cs
+++ Autores_Libros.DaraAccess/Models/AutoresLibrosContext.cs
@@ -25,7 +25,7 @@ namespace Autores_Libros.DaraAccess.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=VM_DBPELDEV\\PELDEV;Database=AutoresLibros;user=sa;password=P0pular21");
+                optionsBuilder.UseSqlServer("Server=;Database=AutoresLibros;user=;password=");
             }
         }
 
